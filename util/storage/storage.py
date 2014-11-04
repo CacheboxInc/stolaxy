65c0d307cf1c80319c627a9a10513981daf8d8e6
Murali Nagaraj <murali.nagaraj@cachebox.com>
2014-11-04 17:56:09 +0530

    * intial code for storage initialization - handles new device   detection, volume group creation, volume creation and extending the   flash tier volume if new devices are detected .

--- util/storage/storage.py
+++ util/storage/storage.py
@@ -100,6 +100,54 @@ class Storage(object):
 
         # TBD. create volume 
 
+        cmd = (
+            "lvdisplay",
+            "-C",
+            "--noheadings",
+            "--separator=','",
+            "--units",
+            "b"
+            )
+
+        lvdisplay = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        out, err = lvdisplay.communicate()
+        if lvdisplay.returncode != 0:
+            print 'WARNING: error running lvdisplay: %s, %s' % (out, err)
+            return
+
+        found = False
+
+        for lvline in out.splitlines():
+            lvline = lvline.split(',')
+            if lvline[0] == STOLAXY_FLASH_TIER_VOLUME:
+                assert lvline[1] == STOLAXY_FLASH_TIER_VOLUME_GROUP
+                found = True
+                break
+
+        if not found:
+            cmd = (
+                "lvcreate",
+                "-n",
+                STOLAXY_FLASH_TIER_VOLUME,
+                "-l",
+                "100%FREE",
+                STOLAXY_FLASH_TIER_VOLUME_GROUP
+                )
+        else:
+            cmd = (
+                "lvextend",
+                "-l",
+                "100%FREE",
+                "/dev/%s/%s" % (
+                    STOLAXY_FLASH_TIER_VOLUME_GROUP, 
+                    STOLAXY_FLASH_TIER_VOLUME)
+                )
+                
+        op = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        out, err = op.communicate()
+        if op.returncode != 0:
+            print 'WARNING: error running lvcreate/lvextend: %s, %s' % (out, err)
+            return
         print 'storage layer initialized successfully'
         
 if __name__ == '__main__':
